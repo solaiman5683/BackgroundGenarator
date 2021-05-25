5d70d401f6dd025c8b030b921bae6e578f266dba
Muhammad Sulaiman Hosain <55309044+solaiman5683@users.noreply.github.com>
2021-05-25 14:32:13 +0600

    backgroundGenarator

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const body = document.querySelector('body');
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const css = document.querySelector("h3");
+const fontColor = document.querySelector("#fontColor");
+const options = document.querySelector('select');
 
-function colorChange(){
-    body.style.background = `linear-gradient(to right, ${color1.value},${color2.value})`;
-    console.log(color1.value)
+function colorChange() {
+    body.style.background = `linear-gradient(${options.value}, ${color1.value},${color2.value})`;
+    css.textContent = body.style.background + ";";
 }
 
 console.log(color1.value);
 
 
-color1.addEventListener("input", colorChange)
-color2.addEventListener("input", colorChange)
+color1.addEventListener("input", colorChange);
+color2.addEventListener("input", colorChange);
+options.addEventListener("input", colorChange);
+fontColor.addEventListener("input", function () {
+    body.style.color = fontColor.value;
+})
